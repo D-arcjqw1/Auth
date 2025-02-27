0ebd9cd700a6a83d02db8d0bc99a866fe757c207
DDD <117805126+D-arcjqw1@users.noreply.github.com>
2025-02-27 22:00:28 +0300

    bosin3.1

--- grpc/cmd/grpc_server/main.go
+++ grpc/cmd/grpc_server/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/D-arcjqw1/Auth/api/user"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type UserService struct {
+	pb.UnimplementedUserServiceServer
+}
+
+func (s *UserService) Create(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	log.Printf("Создан пользователь: %s, %s", req.Name, req.Email)
+	return &pb.CreateUserResponse{Id: time.Now().Unix()}, nil
+}
+
+func (s *UserService) Get(ctx context.Context, req *pb.GetUserRequest) (*pb.GetUserResponse, error) {
+	log.Printf("Получение пользователя с ID: %d", req.Id)
+	return &pb.GetUserResponse{
+		Id:        req.Id,
+		Name:      "Test User",
+		Email:     "test@example.com",
+		Role:      pb.Role_USER,
+		CreatedAt: nil,
+		UpdatedAt: nil,
+	}, nil
+}
+
+func (s *UserService) Update(ctx context.Context, req *pb.UpdateUserRequest) (*emptypb.Empty, error) {
+	log.Printf("Обновление пользователя ID: %d", req.Id)
+	return &emptypb.Empty{}, nil
+}
+
+func (s *UserService) Delete(ctx context.Context, req *pb.DeleteUserRequest) (*emptypb.Empty, error) {
+	log.Printf("Удаление пользователя ID: %d", req.Id)
+	return &emptypb.Empty{}, nil
+}
